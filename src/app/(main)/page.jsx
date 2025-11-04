a823046263bfd892d53366e27cf6c7995269cdbc
osakdv <osakdv@gmail.com>
2025-11-04 20:10:40 +0100

    refactor: clean up unused imports and improve component structure

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,3 @@
-import BookingFlow from "@/components/booking_flow/Booking_flow";
-import ExperiencesSection from "../customer_landingpage/ExperiencesSection";
-import BecomeVendorSection from "../customer_landingpage/Become_a_vendorSection";
-import ExploreCarsSection from "../customer_landingpage/Explore_cars";
-import ExploreDiningSection from "../customer_landingpage/Explore_dining";
-import ExploreBeachSection from "../customer_landingpage/Expore_beaches";
-import FaqSection from "../customer_landingpage/Faqs";
-
 import HomePageHero from "../customer_landingpage/components/HeroSection";
 import AboutService from "../customer_landingpage/components/AboutService";
 import OtherServicesLinks from "../customer_landingpage/components/OtherServicesLinks";
